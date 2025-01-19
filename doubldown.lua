e9c35bf8d183bc6912423ac6261521c309473ec3
RobScriper <anperchik3@gmail.com>
2025-01-19 11:26:52 +0200

    Update doubldown.lua

--- doubldown.lua
+++ doubldown.lua
@@ -1,39 +1,44 @@
 local http = require("socket.http")
 local ltn12 = require("ltn12")
-local json = require("dkjson") -- Убедитесь, что у вас установлена эта библиотека для работы с JSON.
+local json = require("dkjson") -- Для кодирования JSON
 
--- URL вашего вебхука Discord
+-- Ваш вебхук Discord
 local webhookURL = "https://discord.com/api/webhooks/1330467876829794355/7MScs2cJO59fyTLofXCHVS_8j6Fg8dKfkzri428_0jb80_3qqdLHUGNUTbhSPBfU_YVn"
 
--- Создаём данные для отправки
+-- Сообщение для отправки
 local data = {
     username = "Lua Bot", -- Имя отправителя
-    content = "Привет! Это сообщение отправлено через Lua." -- Сообщение
+    content = "Привет! Это тестовое сообщение через Lua." -- Основное сообщение
 }
 
 -- Преобразуем таблицу в JSON
 local jsonData, err = json.encode(data)
-if err then
-    print("Ошибка кодирования JSON: " .. err)
+if not jsonData then
+    print("Ошибка при кодировании JSON: " .. err)
     return
 end
 
 -- Настраиваем HTTP-запрос
 local response = {}
-local res, code, headers = http.request {
-    url = webhookURL,
-    method = "POST",
-    headers = {
-        ["Content-Type"] = "application/json",
-        ["Content-Length"] = tostring(#jsonData),
-    },
-    source = ltn12.source.string(jsonData),
-    sink = ltn12.sink.table(response),
-}
+local success, statusCode = pcall(function()
+    return http.request{
+        url = webhookURL,
+        method = "POST",
+        headers = {
+            ["Content-Type"] = "application/json",
+            ["Content-Length"] = tostring(#jsonData)
+        },
+        source = ltn12.source.string(jsonData),
+        sink = ltn12.sink.table(response)
+    }
+end)
 
--- Выводим результат
-if code == 200 then
+-- Проверяем результат
+if not success then
+    print("Ошибка отправки запроса.")
+elseif statusCode == 204 then
     print("Сообщение успешно отправлено!")
 else
-    print("Ошибка отправки: " .. tostring(code))
+    print("Ошибка! Код ответа: " .. tostring(statusCode))
+    print("Ответ сервера: " .. table.concat(response))
 end
